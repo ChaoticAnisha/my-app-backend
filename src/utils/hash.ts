ad28569ab153e3cbfbeec7d6ffbd6a55419cac2f
ChaoticAnisha <anishashah0117>
2026-01-05 10:04:24 +0545

    Auth routes and controller

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,7 +1,6 @@
 import bcrypt from 'bcrypt';
 import { ENV } from '../config/env';
 
-// Hash a plain text password
 export const hashPassword = async (plain: string): Promise<string> => {
   const salt = await bcrypt.genSalt(ENV.BCRYPT_SALT_ROUNDS);
   return bcrypt.hash(plain, salt);
